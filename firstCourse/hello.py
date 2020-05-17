c9cf5f5b1aa5ffa0ed6a8d20fd57c8f577450062
刘刚(liugang02) <liugang02@qding.me>
2020-05-17 09:55:29 +0800

    add 标量对象

--- firstCourse/hello.py
+++ firstCourse/hello.py
@@ -3,3 +3,25 @@ print("Hello Python")
 # 编写和调试程序
 # 语法词汇高亮显示
 # 创建、编辑程序文件
+print("hello world")
+
+a=3
+# 定时器
+import time
+time.sleep(3)
+a=4
+print(a)
+
+
+print(a)
+
+
+# 标量对象
+# init 整数
+# float 实数 16.0
+# bool 布尔 True False
+# NoneType 空类型
+x="this is liu Gang"
+y="he is a handsome boy"
+print(x+","+y)
+

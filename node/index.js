18600bd1be5bfa272040b60874d1ff61750a7063
刘刚(liugang02) <liugang02@qding.me>
2020-05-17 14:24:57 +0800

    add node

--- node/index.js
+++ node/index.js
@@ -1,21 +1,23 @@
 var chokidar = require('chokidar');
+var Path = require("path")
 var watcher = chokidar.watch('../firstCourse', {
-    ignored: /[\/\\]\./, persistent: true
+  ignored: /[\/\\]\./, persistent: true
+});
+
+var exec = require('child_process').exec;
+function execute(cmd) {
+  // console.log(cmd)
+  exec(cmd, function (error, stdout, stderr) {
+    if (error) {
+      console.error(error);
+    }
+    else {
+      console.log(stdout);
+    }
   });
 
-  var exec = require('child_process').exec;
-  function execute(cmd){
-      console.log(cmd)
-      exec(cmd, function(error, stdout, stderr) {
-          if(error){
-              console.error(error);
-          }
-          else{
-            //   console.log("success");
-          }
-      });
-      
-    }
-    watcher.on("change",(path)=>{
-        execute("python "+path)
-  })
+}
+watcher.on("change", (path) => {
+  let cmd = "python " + Path.resolve(path);
+  execute(cmd)
+})
